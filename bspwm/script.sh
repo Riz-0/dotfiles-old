b65b51dd1243a4f714c59405ffe297c4df49377b
riz <55377396+Riz-0@users.noreply.github.com>
2022-04-19 18:57:54 +0800

    Steam games now launch on the game tab

---
+++ bspwm/script.sh
@@ -0,0 +1,6 @@
+mygames=$(ls ~/.steam/steam/steamapps | grep appmanifest | sed 's/[^0-9]*//g')
+# this sed command removes everything but the digits
+
+for game in $mygames; do
+        bspc rule -a steam_app_$game desktop= state=fullscreen
+done
