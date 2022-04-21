d3b14054dc00a4b92adc6d3c03f1637096081638
riz <55377396+Riz-0@users.noreply.github.com>
2022-04-21 20:28:37 +0800

    Some Steam games now launch on the games desktop

--- bspwm/script.sh
+++ bspwm/script.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 mygames=$(ls ~/.steam/steam/steamapps | grep appmanifest | sed 's/[^0-9]*//g')
 # this sed command removes everything but the digits
 
 for game in $mygames; do
-        bspc rule -a steam_app_$game desktop= state=fullscreen
+        bspc rule -a steam_app_$game desktop= state=fullscreen follow=on
 done
